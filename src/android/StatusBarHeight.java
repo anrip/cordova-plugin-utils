19e0a289ccb22a6b9ffeb1a0287a4e79ee13cdc4
若海 <mail@anrip.com>
2019-08-17 10:00:37 +0800

    clear code

--- src/android/StatusBarHeight.java
+++ src/android/StatusBarHeight.java
@@ -2,20 +2,21 @@ package com.anrip.cordova;
 
 import org.json.JSONException;
 
-import android.app.Activity;
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 
+import android.app.Activity;
+import android.util.Log;
+
 public class StatusBarHeight extends CordovaPlugin {
 
     @Override
-    public boolean execute(final String action, final CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
         final Activity activity = this.cordova.getActivity();
 
-        if ("height".equals(action)) {
+        if (action.equals("height")) {
             float height = 0;
             int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
             if (resourceId > 0) {
