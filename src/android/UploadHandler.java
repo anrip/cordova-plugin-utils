fac9f317fe7f04bc0fa65fcb213e83484c4ae1bb
若海 <mail@anrip.com>
2019-08-29 12:32:58 +0800

    优化逻辑，支持多类型

--- src/android/UploadHandler.java
+++ src/android/UploadHandler.java
@@ -8,80 +8,68 @@ import android.webkit.WebChromeClient;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
-
+import java.util.List;
 
 class UploadHandler {
 
+    public static WebChromeClient.FileChooserParams fileChooserParams;
+
     public static Intent createIntent(WebChromeClient.FileChooserParams params) {
+        fileChooserParams = params;
+
         String[] acceptTypes = params.getAcceptTypes();
+        String acceptType = Arrays.toString(acceptTypes);
 
-        String acceptType = acceptTypes[0];
-        if (acceptType.length() > 0) {
-            acceptType = acceptType.split("/")[0];
+        List<Intent> intentList = new ArrayList<Intent>();
+
+        if (acceptType.isEmpty() || acceptType.contains("image/")) {
+            intentList.add(createCameraIntent());
         }
 
-        // Specified 'image/*'
-        if (acceptType.equals("image")) {
-            if (params.isCaptureEnabled()) {
-                return createCameraIntent();
-            }
-            Intent chooser = createChooserIntent(createCameraIntent());
-            chooser.putExtra(Intent.EXTRA_INTENT, params.createIntent());
-            return chooser;
+        if (acceptType.isEmpty() || acceptType.contains("video/")) {
+            intentList.add(createCamcorderIntent());
         }
 
-        // Specified 'video/*'
-        if (acceptType.equals("video")) {
-            if (params.isCaptureEnabled()) {
-                return createCamcorderIntent();
-            }
-            Intent chooser = createChooserIntent(createCamcorderIntent());
-            chooser.putExtra(Intent.EXTRA_INTENT, params.createIntent());
-            return chooser;
+        if (acceptType.isEmpty() || acceptType.contains("audio/")) {
+            intentList.add(createSoundRecorderIntent());
         }
 
-        // Specified 'audio/*'
-        if (acceptType.equals("audio")) {
-            if (params.isCaptureEnabled()) {
-                return createSoundRecorderIntent();
-            }
-            Intent chooser = createChooserIntent(createSoundRecorderIntent());
-            chooser.putExtra(Intent.EXTRA_INTENT, params.createIntent());
-            return chooser;
-        }
+        Intent[] intents = intentList.toArray(new Intent[intentList.size()]);
+        return createChooserIntent(intents);
+    }
 
-        // No special, trigger the default file upload chooser.
-        Intent chooser = createChooserIntent(createCameraIntent(), createCamcorderIntent(), createSoundRecorderIntent());
-        chooser.putExtra(Intent.EXTRA_INTENT, params.createIntent());
+    private static Intent createChooserIntent(Intent[] intents) {
+        Intent chooser = new Intent(Intent.ACTION_CHOOSER);
+        chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, intents);
+        chooser.putExtra(Intent.EXTRA_INTENT, fileChooserParams.createIntent());
         return chooser;
     }
 
-    private static Intent createChooserIntent(Intent... intents) {
-        Intent chooser = new Intent(Intent.ACTION_CHOOSER);
-        chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, intents);
-        return chooser;
+    private static File createCaptureFile(String ext) {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return new File(imagesDir.getPath() + "/" + timeStamp + "." + ext);
     }
 
     private static Intent createCameraIntent() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(createImageFile()));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(createCaptureFile("jpg")));
         return intent;
     }
 
     private static Intent createCamcorderIntent() {
-        return new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 15);
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0);
+        return intent;
     }
 
     private static Intent createSoundRecorderIntent() {
         return new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
-    }
-
-    private static File createImageFile() {
-        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        return new File(storageDir.getPath() + "/" + timeStamp + ".jpg");
     }
 
 }
